6441153ec3f7bec9a1710a64eeb4531852a29653
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-19 18:08:24 -0400

    Added local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
+import { loadState, saveState } from './localStorage.js';
 import reducers from '../reducers';
 
-export default createStore(reducers, applyMiddleware(reduxThunk));
+const key = 'state';
+const persistedState = loadState(key);
+const store = createStore(
+  reducers,
+  persistedState,
+  applyMiddleware(reduxThunk)
+);
+
+store.subscribe(() => {
+  const currentState = store.getState();
+  saveState(currentState, key);
+});
+
+export default store;
